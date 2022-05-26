1f9527b8b0ee25bb2afd2747531db8a2131f7a45
Bhavesh Lad <lad.bhavesh@gmail.com>
2022-05-26 10:21:57 +0530

    Add project files.

---
+++ TestApp/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return $"Time now is {DateTime.Now}";
+        }
+    }
+}
